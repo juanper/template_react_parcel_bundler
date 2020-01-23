c56c834a2941206e73278196567b1e023d90783f
Juan José Pereira <juanper@uol.com.br>
2020-01-23 11:32:32 -0300

    Adicionar imagem e css em teste

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './index.css'
+import logo from './index.jpeg'
 
-const App = () => <h1>Parcel Bundler with React</h1>
+const App = () => <div>
+  <h1>Parcel Bundler with React</h1>
+  <img src={logo}></img>
+  </div>
 
 render(
   <App/>,
